d8eae4ee1d83eb9a1d64f3b0f4cba5c393702fd7
Laís Cavalcanti <cavalcantislais@gmail.com>
2020-03-17 01:58:18 -0300

    pasta static/assets, plugin imagens para posts e component post pronto

--- blog-subsi/gatsby-config.js
+++ blog-subsi/gatsby-config.js
@@ -11,6 +11,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
+    // needs to be the first to work with gatsby-remark-images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uploads`,
+        path: `${__dirname}/static/assets/img`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -18,19 +26,34 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-      {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-          name: `posts`,
-          path: `${__dirname}/posts`,
-        },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/posts`,
       },
-      {
-        resolve: `gatsby-transformer-remark`,
-        options: {
-          plugins: [],
-        },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-relative-images",
+            options: {
+              name: "uploads",
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 960,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-lazy-load`,
+        ],
       },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
